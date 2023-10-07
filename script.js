815bc6d0412175143650153d06924c9d61159ccb
selbps <sejolabapis@gmail.com>
2023-10-08 05:29:20 +0800

    Create basic 16x16 grid

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
-const grid = document.createElement('div');
-grid.classList.add('grid');
+for (let i = 0; i < 16; i++) {
+    for ( let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        
+        container.appendChild(grid);
+    }
+}
 
-container.appendChild(grid);
+    
+
+
+
+
+
+// let createDivs = function (num) {
+//     for (i = 0; i <= num; i++) {
+//         if (i = 16) {
+//             return;
+//         }
+//     }
+// }
